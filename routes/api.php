d116ab4a2603f65d57d568155622d39c36630cff
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-04 14:14:37 +0530

    route updated

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/reading-items/new', 'ReadingItemController@store');
+Route::resource('/weeklydose', 'ReadingItemController')->only([ 'store' ]);
